cf2c3081bc61e916ffd9d16a620b92559a050956
Dihavide <74509023+Dihavide@users.noreply.github.com>
2021-06-13 12:16:29 -0400

    Added- Linked the buttons in the "view schedule" activity to their respective activities

--- app/src/main/java/com/example/a02_androidproject/ViewScheduleActivity.kt
+++ app/src/main/java/com/example/a02_androidproject/ViewScheduleActivity.kt
@@ -1,12 +1,22 @@
 package com.example.a02_androidproject
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_schedule_view.*
 
 class ViewScheduleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_schedule_view)
+
+        btnAddToSchedule2.setOnClickListener {
+            startActivity(Intent(this,AddToScheduleActivity::class.java))
+        }
+
+        btnDeleteFromSchedule2.setOnClickListener {
+            startActivity(Intent(this,DeleteFromScheduleActivity::class.java))
+        }
 
     }
 }
