e6ba420f5535ac263f07bdd8cf6e07a0e65be491
Kishowth Moganasundaram <kishowth@users.noreply.github.com>
2021-06-12 23:58:00 -0400

    06/12/2021- FINAL COMMIT added: -Brand new WORKING fade-in loading screen -View schedule button works -gradle up to par -Created a second activity for schedule
    Need to:
    -fix homepage colours
    -Add working schedule on master page
    -Implement adding to actual schedule functions

--- app/src/main/java/com/example/a02_androidproject/SplasgScreenActivity.kt
+++ app/src/main/java/com/example/a02_androidproject/SplasgScreenActivity.kt
@@ -1,11 +1,25 @@
 package com.example.a02_androidproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splasg_screen.*
 
 class SplasgScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splasg_screen)
+
+        timerIcon.alpha = 0f
+        timerIcon.animate().setDuration(1500).alpha(1f).withEndAction {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+
+
+
+
     }
 }
