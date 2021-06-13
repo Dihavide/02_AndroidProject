36f41d144b2ee141220233ef97d11635644296df
Dihavide <74509023+Dihavide@users.noreply.github.com>
2021-06-13 11:56:41 -0400

    Added- A new activity for the "add to schedule" buttons, and changed font colors of some text Also added buttons for the new activity that currently do not do anything

---
+++ app/src/main/java/com/example/a02_androidproject/AddToScheduleActivity.kt
@@ -0,0 +1,13 @@
+package com.example.a02_androidproject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class AddToScheduleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
