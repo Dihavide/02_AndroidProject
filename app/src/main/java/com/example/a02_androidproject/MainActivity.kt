afa48b0715e4612ac85939303e396af30744eb7a
Dihavide <74509023+Dihavide@users.noreply.github.com>
2021-06-12 23:22:20 -0400

    Added- Buttons to activity_schedule_view.xml Added a function to go to activity_schedule_view.xml through the main class

--- app/src/main/java/com/example/a02_androidproject/MainActivity.kt
+++ app/src/main/java/com/example/a02_androidproject/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.a02_androidproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnCheckSchedule.setOnClickListener {
+            val intent = Intent(this,ViewScheduleActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
