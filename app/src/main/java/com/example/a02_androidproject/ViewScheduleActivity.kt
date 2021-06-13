fab8f66116887dae510b37b9cb7ee9c0d99c8bc7
Dihavide <74509023+Dihavide@users.noreply.github.com>
2021-06-12 23:07:05 -0400

    Added- A new XML file A new main file for second activity

--- app/src/main/java/com/example/a02_androidproject/ViewScheduleActivity.kt
+++ app/src/main/java/com/example/a02_androidproject/ViewScheduleActivity.kt
@@ -1,13 +1,12 @@
 package com.example.a02_androidproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class ViewScheduleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_schedule_view)
 
     }
 }
