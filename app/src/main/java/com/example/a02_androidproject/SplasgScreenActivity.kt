a79beb654f9981868fb4f24a0e92d3f1c667fc3f
Kishowth Moganasundaram <kishowth@users.noreply.github.com>
2021-06-12 23:23:46 -0400

    added: -started splashScreen activity v2

---
+++ app/src/main/java/com/example/a02_androidproject/SplasgScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a02_androidproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplasgScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splasg_screen)
+    }
+}
