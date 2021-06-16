c1151873630afed0471d00a73261ac9ed993f288
Dihavide <74509023+Dihavide@users.noreply.github.com>
2021-06-15 23:15:28 -0400

    6/15/2021 Added- Created an adapter that may or may not work.  Commented out functions will cause the app to crash

--- app/src/main/java/com/example/a02_androidproject/MainActivity.kt
+++ app/src/main/java/com/example/a02_androidproject/MainActivity.kt
@@ -3,12 +3,24 @@ package com.example.a02_androidproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_schedule_to_add.*
+import kotlinx.android.synthetic.main.activity_schedule_view.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var scheduleAdapter: ScheduleAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        scheduleAdapter = ScheduleAdapter(mutableListOf())
+//        rvScheduleList.adapter = scheduleAdapter
+//        rvScheduleList.layoutManager = LinearLayoutManager(this)
+
 /**
  *This allows the 'btnCheckSchedule' to perform a function upon clicking
  **/
@@ -23,6 +35,24 @@ class MainActivity : AppCompatActivity() {
         btnDeleteFromSchedule.setOnClickListener {
             startActivity(Intent(this,DeleteFromScheduleActivity::class.java))
         }
+
+//        btnAddTextToSchedule.setOnClickListener {
+//            val scheduleContent = etAddToSchedule.text.toString()
+//            if(scheduleContent.isNotEmpty())
+//            {
+//                val scheduleTwo = Schedule(scheduleContent)
+//                scheduleAdapter.addSchedule(scheduleTwo)
+//            }
+//            else
+//            {
+//                Toast.makeText(this, "You have not inputted any parameters", Toast.LENGTH_SHORT).show()
+//
+//            }
+//        }
+
+
+
+
 
     }
 }
--- app/src/main/java/com/example/a02_androidproject/ScheduleAdapter.kt
+++ app/src/main/java/com/example/a02_androidproject/ScheduleAdapter.kt
@@ -6,25 +6,23 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.list_item.view.*
 
-class ScheduleAdapter(private val schedules: MutableList<Schedule>) : RecyclerView.Adapter<ScheduleAdapter.ScheduleViewHolder>(){
-
+class ScheduleAdapter(private val schedules : MutableList<Schedule>) : RecyclerView.Adapter<ScheduleAdapter.ScheduleViewHolder>()
+{
     class ScheduleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ScheduleViewHolder {
-        return ScheduleViewHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.list_item,
-                parent,
-                false
-            )
-        )
+        return ScheduleViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_item,parent,false))
+    }
+
+    fun addSchedule(schedule: Schedule)
+    {
+        schedules.add(schedule)
+        notifyItemInserted(schedules.size-1)
     }
 
     override fun onBindViewHolder(holder: ScheduleViewHolder, position: Int) {
         val curSchedule = schedules[position]
-        holder.itemView.apply {
-            tvScheduleInformation.text = curSchedule.
-        }
+        holder.itemView.tvScheduleInformation.text = curSchedule.schedule
     }
 
     override fun getItemCount(): Int {
