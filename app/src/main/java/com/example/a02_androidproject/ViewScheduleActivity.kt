aebe9b5d7fe6c3202337193dbee7a79ba378bfbb
Dihavide <74509023+Dihavide@users.noreply.github.com>
2021-06-19 12:05:13 -0400

    6/18/2021
    Added-
    Fixed:
    The function in the view schedule class where if you click the add button with an empty edit text box, it will send out the right response

--- app/src/main/java/com/example/a02_androidproject/ViewScheduleActivity.kt
+++ app/src/main/java/com/example/a02_androidproject/ViewScheduleActivity.kt
@@ -33,13 +33,14 @@ class ViewScheduleActivity : AppCompatActivity() {
         btnAddTextToSchedule.setOnClickListener {
 
             //Sets the user input inside the etAddToSchedule space to a string and stores it into a  variable
-            val scheduleContent = "    -  " + etAddToSchedule.text.toString()
+            val scheduleContent = etAddToSchedule.text.toString()
 
             //If user input was given upon the click of the button
             if(scheduleContent.isNotEmpty())
             {
+                val displaySchedule = "    -  " + scheduleContent
                 //Take the user input, run it through the data class,then uses the addSchedule function found in the adapter to add that user input into the array at the last index
-                scheduleAdapter.addSchedule(Schedule(scheduleContent))
+                scheduleAdapter.addSchedule(Schedule(displaySchedule))
                 //Clears any user input that is already inside the etAddToSchedule box
                 etAddToSchedule.text.clear()
                 //Creates a message saying that the input has been added to the schedule
