7369b52e1ae378626ff7792b48eb22e73bb2d15f
Kishowth Moganasundaram <kishowth@users.noreply.github.com>
2021-06-20 23:53:25 -0400

    20/06/2021- FINAL COMMIT
    ADDED:
    -Documentation and other comments throughout code
    -Finalized colour constants
    -all constraints finalized
    
    FIXED:
    -Text size for the add task function
    
    NEED TO FIX:
    -Nothing :)

--- app/src/main/java/com/example/a02_androidproject/SplasgScreenActivity.kt
+++ app/src/main/java/com/example/a02_androidproject/SplasgScreenActivity.kt
@@ -4,13 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_splasg_screen.*
-
+/**
+ * This class implenents the code for the loading screen
+ * Uses the animation features to set a fade effect
+ * */
 class SplasgScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splasg_screen)
 
+        //Init time for timer is 0
         timerIcon.alpha = 0f
+
+        //set duration for 1500ms, always override the current method
+        //fade in then fade out into the app homepage
         timerIcon.animate().setDuration(1500).alpha(1f).withEndAction {
             val i = Intent(this, MainActivity::class.java)
             startActivity(i)
